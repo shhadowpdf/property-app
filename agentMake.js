e4239781c62f0bf048a0f55491432ca429c4c5f4
Saksham Kushwah <89777082+shhadowpdf@users.noreply.github.com>
2025-07-14 13:07:45 +0530

    Project status: Completed

--- agentMake.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-const agent = require("./models/agentData");
-mongoose.connect(process.env.MONGODB_URI).then(()=> console.log("CONNECTED"));
-const Agent = new agent({
-    username: "mukku9669",
-    password: "1309"
-});
-Agent.save().then(()=> console.log("Done"))
